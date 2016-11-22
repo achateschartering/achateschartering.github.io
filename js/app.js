a0b0307a75a624b4a7270823f523c3e53e20d5ee
TOM TOMONARI <tom.tomonari@gmail.com>
2016-11-22 15:52:53 +0800

    Modified details

--- js/app.js
+++ js/app.js
@@ -18,8 +18,18 @@ angular
       },
       dateClick: function(date) {
         $scope.bookDate = date.day + '/' + date._month + '/' + date.year;
-        //$scope.book_btn_href = "https://docs.google.com/a/v-tech.com.sg/forms/d/e/1FAIpQLSf8qVlO3RmcDCp-Zwew5iCm8GkAZf2f6tjGo-ow4BHlY_a82w/viewform?entry.789089950=" + date.year + "-" + date._month + "-" + date.day + "&entry.2055232012&entry.1000020&entry.1000022&entry.1000025&entry.1000023";
-        $scope.book_btn_href = "https://docs.google.com/forms/d/e/1FAIpQLSf8qVlO3RmcDCp-Zwew5iCm8GkAZf2f6tjGo-ow4BHlY_a82w/viewform?entry.789089950=" + date.year + "-" + date._month + "-" + date.day;
+        if(date._month < 10){
+          var monthForm = '0' + date._month;
+        }else{
+          var monthForm = date._month;
+        }
+
+        if(date.day < 10){
+          var dayForm = '0' + date.day;
+        }else{
+          var dayForm = date.day;
+        }
+        $scope.book_btn_href = "https://docs.google.com/forms/d/e/1FAIpQLSf8qVlO3RmcDCp-Zwew5iCm8GkAZf2f6tjGo-ow4BHlY_a82w/viewform?entry.789089950=" + date.year + "-" + monthForm + "-" + dayForm;
         $scope.show_book_btn = true;
       },
       changeMonth: function(month, year) {
